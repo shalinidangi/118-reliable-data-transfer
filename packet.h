91ec9effbd01f08c7a9e64c5b51141d69907053b
binerys <bree.nery@gmail.com>
2017-03-12 13:47:07 -0700

    Add header file for packet

---
+++ packet.h
@@ -0,0 +1,21 @@
+/**
+ * Packet.h
+ * High level representation of a packet 
+ */
+
+#define PACKET_SIZE 1024
+#define PACKET_DATA_SIZE 1000
+#define RETRANSMISSION_TIME_OUT 500
+
+// PACKET TYPES
+#define TYPE_REQUEST 0
+#define TYPE_DATA 1
+#define TYPE_ACK 2
+#define TYPE_END_DATA 3
+
+struct Packet {
+  int sequence; 
+  int type; 
+  int length;
+  char data[PACKET_DATA_SIZE];  
+}
